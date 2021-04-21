3be3c134a497037303a7fad70c99b5153738fff6
Sergejs Zablockis <zablose@gmail.com>
2021-04-21 13:42:43 +0100

    Update DAMP to 2.0.0 and Allog to 2.1.0. PHP required 7.4 or 8.0.

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-use Zablose\Allog\Config;
+use Zablose\Allog\Config\Server as Config;
 use Zablose\Allog\Server;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 (new Server((new Config())->read(__DIR__.'/../.env')))->run();
